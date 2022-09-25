1cc9d760bbe4ec4b69a79b22124682d18936bec1
Jonathan Acosta <jonathanacosta@Jonathans-MacBook-Pro.local>
2022-09-25 17:46:13 -0400

    Placed the contacts as a dictionary on cell_phone.py file. Called contacts on main.py file but only names come up. Created an input function on messages on cell_phone.py file to allow user to write their own message. Also called messages on main.py file

--- cell_phone.py
+++ cell_phone.py
@@ -1,12 +1,12 @@
 class CellPhone:
 
 
-    def __init__(self, cell_phone_model, vibrate_set):
+    def __init__(self, cell_phone_model, vibration_is_set):
         self.cell_phone_model = cell_phone_model
         self.phone_number = 3475559911 
-        # self.contacts = contacts
-        # self.messages = messages
-        self.vibrate = vibrate_set
+        self.contacts = {"Michael": 1234567890, "Jim": 2345678901, "Dwight": 3456789012, "Pam": 4567890123}
+        self.messages = input("Type your message here: ")
+        self.vibrate = vibration_is_set
 
     def kind_of_cell_phone(self, cell_phone_model):
         self.cell_phone_model = cell_phone_model
--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from cell_phone import CellPhone
 my_cell_phone = CellPhone("iphone 14", False)
 print(my_cell_phone.cell_phone_model)
 print(my_cell_phone.phone_number)
+print(my_cell_phone.contacts)
+print(my_cell_phone.messages)
 
 
 if my_cell_phone.vibrate == True:
     print("Cell phone set to vibrate!")
 else:
     print("Cell phone ringer on!")
+
+my_cell_phone.send_text_messages("What up tho")
+print(my_cell_phone.messages)
+
+
+for contacts in my_cell_phone.contacts.keys():
+    print(contacts)
